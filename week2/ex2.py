e95da9f40cbfa61586ca637bd3c3cb5e3bd60357
Nico <nico@schemberg.net>
2016-10-30 17:02:19 +0100

    Adding Functions

--- week2/ex2.py
+++ week2/ex2.py
@@ -3,20 +3,30 @@
 import telnetlib
 import time
 
-remote_conn = telnetlib.Telnet('184.105.247.70', 23, 6)
-#print remote_conn.read_until('sername:', 6)
-remote_conn.write('pyclass' + '\n')
+def open_conn():
+    remote_conn = telnetlib.Telnet('184.105.247.70', 23, 6)
+    #print remote_conn.read_until('sername:', 6)
+    remote_conn.write('pyclass' + '\n')
 
-#print remote_conn.read_until('word:', 6)
-remote_conn.write('88newclass' + '\n')
+    #print remote_conn.read_until('word:', 6)
+    remote_conn.write('88newclass' + '\n')
 
-time.sleep(1)
-print remote_conn.read_very_eager()
+    time.sleep(1)
+    print remote_conn.read_very_eager()
 
+def send_comm():
+    remote_conn.write('show ip int brief' + '\n')
 
-remote_conn.write('show ip int brief' + '\n')
+    time.sleep(1)
+    print remote_conn.read_very_eager()
 
-time.sleep(1)
-print remote_conn.read_very_eager()
+def close_conn():
+    remote_conn.close()
 
-remote_conn.close()
+def main():
+    open_conn()
+    send_comm()
+    close_conn()
+
+if __name__ == __main__
+    main()
