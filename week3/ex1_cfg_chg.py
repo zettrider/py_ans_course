d1191cbf47699b4e45a660fd8f4732c75203859c
Zettrider <nico@schemberg.net>
2016-11-12 11:08:20 -0800

    Started with Basics

--- week3/ex1_cfg_chg.py
+++ week3/ex1_cfg_chg.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python
 
-snmp_oids = (
-#    ('sysName', '1.3.6.1.2.1.1.5.0', None),
-    ('sysUptime', '1.3.6.1.2.1.1.3.0', None),
-    ('runLastChange', '1.3.6.1.4.1.9.9.43.1.1.1.0', True),
-    ('runLastSave', '1.3.6.1.4.1.9.9.43.1.1.2.0', True),
-    ('startLastChange', '1.3.6.1.4.1.9.9.43.1.1.3.0', True),
-)
+'''
+Using SNMPv3 create a script that detects router configuration changes.
 
-IP = '184.105.247.71'
-portv3 = 161
-a_user = 'pysnmp'
-auth_key = 'galileo1'
-encrypt_key = 'galileo1'
-snmp_user = (a_user, auth_key, encrypt_key)
-pynet_rtr = (IP, portv3)
+If the running configuration has changed, then send an email notification to
+yourself identifying the router that changed and the time that it changed.
 
-import snmp_helper
+In this exercise, you will possibly need to save data to an external file. One
+way you can accomplish this is by using a pickle file.
+'''
 
-sysName = snmp_helper.snmp_get_oid_v3(pynet_rtr, snmp_user, oid='1.3.6.1.2.1.1.5.0')
+import cPickle as pickle
+import os.path
+from getpass import getpass
+from datetime import datetime
 
-print snmp_helper.snmp_extract(sysName) + " - " + IP
+from snmp_helper import snmp_get_oid_v3, snmp_extract
+from email_helper import send_mail
 
-for desc, an_oid, is_count in snmp_oids:
-    snmp_data = snmp_helper.snmp_get_oid_v3(pynet_rtr, snmp_user, oid=an_oid)
-    output = snmp_helper.snmp_extract(snmp_data)
-    print "%s    %s" % (output, desc)
+#Used oids
+runLastChange = '1.3.6.1.4.1.9.9.43.1.1.1.0'
+sysName = '1.3.6.1.2.1.1.5.0'
+sysUptime = '1.3.6.1.2.1.1.3.0'
+
+def main():
+
+    # SNMPv3 Connection Parameters
+    rtr1_ip_addr = raw_input("Enter pynet-rtr1 IP: ")
+    rtr2_ip_addr = raw_input("Enter pynet-rtr2 IP: ")
+    my_key = getpass(prompt="Auth + Encryption Key: ")
+
+    a_user = 'pysnmp'
+    portv3 = 161
+    auth_key = my_key
+    encrypt_key = my_key
+
+    snmp_user = (a_user, auth_key, encrypt_key)
+    pynet_rtr1 = (rtr1_ip_addr, portv3)
+    pynet_rtr2 = (rtr2_ip_addr, portv3)
+
+    print "test"
+
+    for a_device in (pynet_rtr1, pynet_rtr2):
+        print "test2"
+        for oid in (sysName, sysUptime, runLastChange):
+            print "test3"
+            print snmp_extract(snmp_get_oid_v3(a_device, snmp_user, oid=oid))
+
+if __name__ == '__main__':
+    main()
