f9ae41bda235868bc31526638e69cc92f1a6346d
Zettrider <nico@schemberg.net>
2016-11-16 02:56:03 -0800

    some lines changed

--- week3/ex1_cfg_chg.py
+++ week3/ex1_cfg_chg.py
@@ -18,12 +18,94 @@ from datetime import datetime
 from snmp_helper import snmp_get_oid_v3, snmp_extract
 from email_helper import send_mail
 
+# Constants
+DEBUG = True
+
+# 300 seconds (converted to hundredths of seconds)
+RELOAD_WINDOWS = 300*100
+
 #Used oids
-runLastChange = '1.3.6.1.4.1.9.9.43.1.1.1.0'
-sysName = '1.3.6.1.2.1.1.5.0'
-sysUptime = '1.3.6.1.2.1.1.3.0'
+RUN_LAST_CHANGED = '1.3.6.1.4.1.9.9.43.1.1.1.0'
+SYS_NAME = '1.3.6.1.2.1.1.5.0'
+SYS_UPTIME = '1.3.6.1.2.1.1.3.0'
+
+def obtain_saved_objects(file_name):
+    '''
+    Read in previously saved objects from a pickle file
+
+    Returns a dict:
+    {
+        'device_name': device_object,
+        'device_name': device_object,
+    }
+
+    '''
+
+    # Check that pickle file exists
+    if not os.path.isfile(file_name):
+        return {}
+
+    # Read in any saved network devices
+    net_devices = {}
+    with open(file_name, 'r') as f:
+        while True:
+            try:
+                tmp_device = pickle.load(f)
+                net_devices[tmp_device.device_name] = tmp_device
+            except EOFError:
+                break
+
+    return net_devices
+
+class NetworkDevice(object):
+    '''
+    Simple object to store network device information
+
+    For an alternate solution, you could replace the class/objects with
+    a data structure that uses dictionaries.
+    '''
+
+
+    def __init__(self, device_name, uptime, last_changed, config_changed=False):
+        self.device_name = device_name
+        self.uptime = uptime
+
+        # The uptime value in hundredths of seconds when running configuration
+        # was last changed
+        self.last_changed = last_changed
+        self.run_config_changed = config_changed
+
 
 def main():
+
+    '''
+    Check if the running-configuration has changed, send an email notification when
+    this occurs.
+
+    Logic for detecting the running-config has changed:
+
+        Normal (non-reboot):
+
+            #Did RUN_LAST_CHANGED increase
+            if RUN_LAST_CHANGED > network_device_object.last_changed:
+                config_changed = True
+
+        Reboot case:
+
+            RUN_LAST_CHANGED decreases (i.e. < network_device_object.last_changed)
+
+            Right after reboot, RUN_LAST_CHANGED is updated upon
+            load of running-config from startup-config.
+
+            Create a grace window (RELOAD_WINDOW) for values of RUN_LAST_CHANGED.
+            In other words as long as RUN_LAST_CHANGED is <= RELOAD_WINDOWS assume
+            no running-config changes.
+
+            If RUN_LAST_CHANGED is > RELOAD_WINDOW assume running-config was changed
+    '''
+
+    # Pickle file for storing previous RunningLastChanged timestamp
+    net_dev_file = 'netdev.pk1'
 
     # SNMPv3 Connection Parameters
     rtr1_ip_addr = raw_input("Enter pynet-rtr1 IP: ")
@@ -39,11 +121,13 @@ def main():
     pynet_rtr1 = (rtr1_ip_addr, portv3)
     pynet_rtr2 = (rtr2_ip_addr, portv3)
 
-    print "test"
+    print '\n*** Checking for device changes ***'
+    saved_devices = obtain_saved_objects(net_dev_file)
+    print "{0} devices were previously saved\n".format(len(saved_devices))
 
     for a_device in (pynet_rtr1, pynet_rtr2):
         print "test2"
-        for oid in (sysName, sysUptime, runLastChange):
+        for oid in (SYS_NAME, SYS_UPTIME, RUN_LAST_CHANGED):
             print "test3"
             print snmp_extract(snmp_get_oid_v3(a_device, snmp_user, oid=oid))
 
